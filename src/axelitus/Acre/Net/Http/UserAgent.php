bf2941ad98ec2022414a360c0b46632fb1999ec7
Axel Pardemann <axelitus2005@gmail.com>
2012-12-07 18:34:51 -0600

    Adde methods to manage Transports

--- src/axelitus/Acre/Net/Http/UserAgent.php
+++ src/axelitus/Acre/Net/Http/UserAgent.php
@@ -12,6 +12,8 @@
 
 namespace axelitus\Acre\Net\Http;
 
+use axelitus\Acre\Common\Str as Str;
+
 /**
  * UserAgent Class
  *
@@ -79,6 +81,95 @@ class UserAgent
      */
     protected function __construct()
     {
+    }
+
+    public function loadTransport($name, $transport_type, $options = null, $use_defaults = true, $replace = false, $set_as_default = false)
+    {
+        if ($this->hasTransport($name) and !$replace) {
+            throw new \RuntimeException("There is already a transport with tht name loaded.");
+        }
+
+        if (is_object($transport_type) and $transport_type instanceof Transport) {
+            // If a Transport was given, change the user agent to this instance
+            $transport_type->setUserAgent($this);
+            $this->transports[$name] = $transport_type;
+        } else {
+            if (!is_string($transport_type) or $transport_type == '') {
+                throw new \InvalidArgumentException("The \$transport_type must be a Transport instance or a non-empty string.");
+            }
+
+            $class = __NAMESPACE__.'\Transport_'.$transport_type;
+            if (!class_exists($class)) {
+                throw new \RuntimeException("The Transport {$class} does not exist.");
+            }
+
+            $this->transports[$name] = $class::forge($this, $options, $use_defaults);
+
+            // Verify if the transport was correctly loaded
+            if (!is_object($this->transports[$name]) or !($this->transports[$name] instanceof Transport)) {
+                throw new \RuntimeException("The Transport could not be loaded.");
+            }
+        }
+
+        $transport = $this->getTransport($name);
+        if ($transport === null) {
+            throw new \RuntimeException("An error occurred and the Transport was not correctly loaded.");
+        }
+
+        $this->default_transport = ($this->default_transport === null or $set_as_default) ? $transport : $this->default_transport;
+
+        return $transport;
+    }
+
+    public function hasTransport($name)
+    {
+        if (!is_string($name) or $name == '') {
+            throw new \InvalidArgumentException("The \$name param must be a non-empty string.");
+        }
+
+        if (array_key_exists($name, $this->transports)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function getTransport($name, $default = null)
+    {
+        if ($this->hasTransport($name)) {
+            return $this->transports[$name];
+        }
+
+        return $default;
+    }
+
+    public function removeTransport($name)
+    {
+        $transport = null;
+        if ($this->hasTransport($name)) {
+            $transport = $this->transports[$name];
+            unset($this->transports[$name]);
+
+            if ($this->default_transport == $transport) {
+                $this->default_transport = (($default = reset($this->transports)) === false) ? null : $default;
+            }
+        }
+
+        return $transport;
+    }
+
+    public function getTransports($as_objects = false)
+    {
+        if($as_objects) {
+            return $this->transports;
+        } else {
+            $transports = array();
+            foreach($this->transports as $name=>$obj) {
+                $transports[$name] = Str::replace(get_class($obj), __NAMESPACE__.'\Transport_', '');
+            }
+
+            return $transports;
+        }
     }
 
     /**
