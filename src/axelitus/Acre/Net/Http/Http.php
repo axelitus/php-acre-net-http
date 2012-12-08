d599e8314735e7239fa2a255494f35b5a3cd5168
Axel Pardemann <axelitus2005@gmail.com>
2012-12-07 18:36:28 -0600

    Added a class to hold the version number

---
+++ src/axelitus/Acre/Net/Http/Http.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Part of the axelitus\Acre\Net\Http Package.
+ *
+ * @package     axelitus\Acre\Net\Http
+ * @version     0.1
+ * @author      Axel Pardemann (dev@axelitus.mx)
+ * @license     MIT License
+ * @copyright   2012 - Axel Pardemann
+ * @link        http://axelitus.mx/
+ */
+
+namespace axelitus\Acre\Net\Http;
+
+
+/**
+ * Http Class
+ *
+ * @see         http://www.ietf.org/rfc/rfc2616.txt     IETF RFC2616 Hypertext Transfer Protocol -- HTTP/1.1 (Sections 6.1.1 and 10)
+ * @package     axelitus\Acre\Net\Http
+ * @category    Net\Http
+ * @author      Axel Pardemann (dev@axelitus.mx)
+ */
+class Http
+{
+    const VERSION = '0.5.0';
+}
